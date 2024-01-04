dbf28a03b40a538bc307dea67587b0d3a33b516a
Nomee07 <gigabanomfundo@gmail.com>
2024-01-04 03:34:05 -0800

    Binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -15,11 +15,11 @@
  */
 struct binary_tree_s
 {
-    int n;
-    int value;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+int n;
+int value;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -44,5 +44,11 @@ size_t binary_tree_size(const binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif
